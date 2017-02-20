5eb4e4e9ed3a27b946e4fded52b31d7cc41ac78a
sitj <sitj9228@yahoo.com>
2017-02-20 17:34:19 -0600

    Added Pause Menu, Scoring System, UI adjustments, Controls

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour {
+
+	public int pointsToAdd;
+
+	// Use this for initialization
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.GetComponent<PlayerControl> () == null)
+			return;
+		ScoreManager.AddPoints (pointsToAdd);
+
+		Destroy (gameObject);
+	}
+}
